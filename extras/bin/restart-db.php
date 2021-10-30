728aa11fe2d68f05eb37061e1c973afd6ee3abc3
Enmanuel Mestanza J <emijacobo@gmail.com>
2021-10-30 02:23:07 -0500

    vehicle first page saved into DB

--- extras/bin/restart-db.php
+++ extras/bin/restart-db.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 require __DIR__ . '/../../src/app/app.php';
+use GuzzleHttp\Client;
 
 try {
     $db = $container->get('settings')['db'];
@@ -27,6 +28,100 @@ try {
     $sql = file_get_contents(__DIR__ . '/../../database/db.sql');
     $pdo->exec($sql);
     echo '[OK] tables inserted successfully' . PHP_EOL;
+
+
+////////////IMporting swapi data
+
+$client = new Client([
+    // Base URI is used with relative requests
+    'base_uri' => 'http://httpbin.org',
+    // You can set any number of default request options.
+    'timeout'  => 2.0,
+]);
+
+// Create a client with a base URI
+$client = new GuzzleHttp\Client(['base_uri' => 'https://swapi.dev/api/']);
+// Send a request to https://foo.com/api/test
+$response = $client->request('GET', 'vehicles');
+
+if ($response->getStatusCode() == 200){
+    $body = $response->getBody();
+    $body = (string)$body;
+    $bodyArr = json_decode($body, true);
+    $ended = false;
+
+    $insertStment = "INSERT INTO teachaway2.vehicle
+    (name, model, manufacturer, cost_in_credits, `length`, max_atmosphering_speed, crew, passengers, cargo_capacity, consumables, vehicle_class, pilots, films, created, edited, url, count)
+    VALUES ";
+
+    $aux = [];
+
+    while (!$ended){
+        $next = $bodyArr["next"];
+
+        if (is_null($next)){
+            $next = true;
+            continue;
+        }
+
+        foreach($bodyArr["results"] as $vehicle){
+
+            // getting the last registered user
+            $stmt = $pdo->query("select CONVERT_TZ('".$vehicle["created"]."', '+00:00','+00:00') as created;");
+            $created = $stmt->fetch()["created"];
+
+            $stmt = $pdo->query("select CONVERT_TZ('".$vehicle["edited"]."', '+00:00','+00:00') as edited;");
+            $edited = $stmt->fetch()["edited"];
+
+             array_push($aux, "('".
+            implode("', '", 
+                [
+                    $vehicle["name"],
+                    $vehicle["model"],
+                    $vehicle["manufacturer"],
+                    $vehicle["cost_in_credits"],
+                    $vehicle["length"],
+                    $vehicle["max_atmosphering_speed"],
+                    $vehicle["crew"],
+                    $vehicle["passengers"],
+                    $vehicle["cargo_capacity"],
+                    $vehicle["consumables"],
+                    $vehicle["vehicle_class"],
+                    implode(",", $vehicle["pilots"]),
+                    implode(",", $vehicle["films"]),
+                    $created,
+                    $edited,
+                    $vehicle["url"],
+                    0
+                ]
+            )."')");
+
+           
+        }
+
+
+        //you must replace endpoint with next value with query string
+        $response = $client->request('GET', 'vehicles');
+
+        if ($response->getStatusCode() == 200){
+            $body = $response->getBody();
+            $body = (string)$body;
+        
+            $bodyArr = json_decode($body, true);
+        }
+
+
+       
+    }
+
+    $insertStment = $insertStment.implode(",",$aux);
+    $pdo->exec($insertStment);
+
+    echo '[OK] SWAPI data imported successfully' . PHP_EOL;
+
+}
+
+
 } catch (PDOException $exception) {
     echo '[ERROR] ' . $exception->getMessage() . PHP_EOL;
 }

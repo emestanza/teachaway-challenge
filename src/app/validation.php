27c90292d662f4a8b5dbdfbb5958ab4a0874d8d6
Enmanuel Mestanza J <emijacobo@gmail.com>
2021-10-31 12:28:29 -0500

    Endpoint to set initial quantity for vehicles and starships

--- src/app/validation.php
+++ src/app/validation.php
@@ -10,3 +10,18 @@ function validate($args){
         throw new ValidationException($v->errors());
     } 
 }
+
+function validatePutForSet($args){
+    $v = new Valitron\Validator($args);
+    $v->rule('required', 'quantity');
+    $v->rule('required', 'id');
+    $v->rule('integer', 'quantity', true);
+    $v->rule('min', 'quantity', 1);
+    $v->rule('integer', 'id', true);
+    $v->rule('min', 'id', 1);
+    $v->rule('in', 'type', ['vehicle', 'starship']);
+
+    if(!$v->validate()) {
+        throw new ValidationException($v->errors());
+    } 
+}
